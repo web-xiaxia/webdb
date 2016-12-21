5a85e882ebf3027cbd54b8ca4d3a642e0c8f8da9
熊闯 <xiongchuang@liziba.com>
2016-12-21 21:03:49 +0800

    暂存

--- html/login.php
+++ html/login.php
@@ -11,16 +11,44 @@
             </td>
         </tr>
         <tr>
-            <td style="width: 20%;min-width:80px;text-align:  center">地址:</td>
-            <td><input style="width: 95%" id="mysql_server_name" value=""></td>
+            <td class="logintitle">地址:</td>
+            <td><input  class="logincontent" id="mysql_server_name" value=""></td>
         </tr>
         <tr>
-            <td style="width: 20%;text-align:  center">用户名:</td>
-            <td><input style="width: 95%" id="mysql_username" value=""></td>
+            <td class="logintitle">用户名:</td>
+            <td><input class="logincontent"  id="mysql_username" value=""></td>
         </tr>
         <tr>
-            <td style="width: 20%;text-align:  center"> 密码:</td>
-            <td><input style="width: 95%" type="password" id="mysql_password" value=""></td>
+            <td class="logintitle"> 密码:</td>
+            <td><input class="logincontent" type="password" id="mysql_password" value=""></td>
+        </tr>
+        <tr>
+            <td class="logintitle">ssh隧道</td>
+            <td  class="logincontent"><input type="checkbox" id="connssh"></td>
+        </tr>
+        <tr>
+            <td class="logintitle">地址：</td>
+            <td  class="logincontent"><input type="text" id=""></td>
+        </tr>
+        <tr>
+            <td class="logintitle">端口：</td>
+            <td  class="logincontent"><input type="text" id=""></td>
+        </tr>
+        <tr>
+            <td class="logintitle">用户名：</td>
+            <td  class="logincontent"><input type="text" id=""></td>
+        </tr>
+        <tr>
+            <td class="logintitle">加密方式：</td>
+            <td  class="logincontent"><input type="checkbox" id="">普通<input type="checkbox" id="">密钥</td>
+        </tr>
+        <tr>
+            <td class="logintitle">密钥：</td>
+            <td  class="logincontent"><input type="text" id=""></td>
+        </tr>
+        <tr>
+            <td class="logintitle">密码：</td>
+            <td  class="logincontent"><input type="password" id=""></td>
         </tr>
         <tr>
             <td><input type="checkbox" id="connsub"></td>
