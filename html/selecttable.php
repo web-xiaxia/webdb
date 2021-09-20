215aaa87f63487adbc4ec92d70367ec99ad2910c
熊闯 <xiongchuang@vip.qq.com>
2021-09-20 13:26:00 +0800

    调整样式

--- html/selecttable.php
+++ html/selecttable.php
@@ -1,14 +1,18 @@
 <div id="tables" style="display: none;" class="window-box">
-    <div style="padding: 35px 0 0;" class="window">
-        <div class="title" style="margin: -35px 0 0;">选择数据表</div>
-        <div style="height: 100%;box-sizing:border-box;padding: 90px 0 0;">
-            <div style="height:73px ; margin: -90px 0 0;">
-                <table width="100%">
+    <div class="window">
+        <div class="title">
+            选择数据表
+            <a href="javascript:btnHash('#databases')"  class="iconfont window-title-btn">&#xe603;</a>
+            <a href="javascript:btnHash('#tabledata2')" class="iconfont window-title-btn">&#xe600;</a>
+        </div>
+        <div style="height: 100%;box-sizing:border-box;padding: 42px 0 0;">
+            <div style="height:36px ; margin: -42px 0 0;">
+                <!-- <table width="100%">
                     <tr>
                         <td><a href="javascript:btnHash('#databases')" class="btn">切换数据库</a></td>
                         <td><a href="javascript:btnHash('#tabledata2')" class="btn">自定义sql</a></td>
                     </tr>
-                </table>
+                </table>-->
                 <input type="text" id="tabledata_input" class="btn"
                        style="width: 80%; margin: 5px auto 0px; background: white; color: black;"/>
             </div>
