86775005adbf9b4aebb459359c499c1d5a11ae74
熊闯 <xiongchuang@vip.qq.com>
2021-09-20 13:19:20 +0800

    调整样式

--- html/selectdatabase.php
+++ html/selectdatabase.php
@@ -1,10 +1,9 @@
 <div id="databases" style="display: none;" class="window-box">
-    <div style="height: 100%; padding:35px 0 0;" class="window">
-        <div class="title" style="margin: -35px 0 0;">选择数据库</div>
-        <div style="height: 100%;box-sizing:border-box;padding: 36px 0 0;">
-            <div style="height:22px ;box-sizing:border-box; margin: -36px 0 0;">
-                <a href="#login" style="background: #005EFF" class="btn">切换数据库连接</a>
-            </div>
+    <div class="window">
+        <div class="title">选择数据库
+            <a href="#login" class="iconfont window-title-btn">&#xe627;</a>
+        </div>
+        <div style="height: 100%;box-sizing:border-box;">
             <ul id="databasesList" style="height: 100%;box-sizing:border-box;overflow: scroll;">
             </ul>
         </div>
