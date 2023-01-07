88cbd4f1d69ca9aa454aae81c6cec9ed1b4bdd39
熊闯 <xiongchuang@vip.qq.com>
2023-01-07 14:40:23 +0800

    调整 记录的信息

--- manifest.php
+++ manifest.php
@@ -1,17 +1,15 @@
-<?php
-echo '{
-  "orientation": "portrait",
-  "background_color": "#ffffff",
-  "theme_color": "#ffffff",
-  "description": "mysql",
-  "display": "standalone",
-  "icons": [
-    {
-      "src": "/webdb/favicon.ico"
-    }
-  ],
-  "name": "Mysql",
-  "short_name": "Mysql",
-  "start_url": "/webdb/?startId=' . md5(uniqid(mt_rand(), true)) . '"
-}'
-?>
+{
+"orientation": "portrait",
+"background_color": "#ffffff",
+"theme_color": "#ffffff",
+"description": "mysql",
+"display": "standalone",
+"icons": [
+{
+"src": "/webdb/favicon.ico"
+}
+],
+"name": "Mysql",
+"short_name": "Mysql",
+"start_url": "/webdb/?startId=<?php echo md5(uniqid(mt_rand(), true)) ?>"
+}
